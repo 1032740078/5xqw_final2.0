ed5b9c9013013a975023e8fe8eab274aea03ac3d
1032740078 <32318856+1032740078@users.noreply.github.com>
2018-05-07 18:11:13 +0800

    Initial commit

---
+++ login-trip/js/login.js
@@ -0,0 +1,101 @@
+/*点击注册账号跳转至注册界面*/
+//function toReg() {
+//	mui.openWindow({
+//		url : "reg.html",
+//		styles:{
+//                      top:'44px',//新页面顶部位置
+//                      bottom:'0px',//新页面底部位置
+//                  },
+//                  show:{
+//                    autoShow:true,//页面loaded事件发生后自动显示，默认为true
+//                    aniShow:'slide-in-right',//页面显示动画，默认为”slide-in-right“；
+//                    duration:'1000'//页面动画持续时间，Android平台默认100毫秒，iOS平台默认200毫秒；
+//	}
+//});
+//}
+
+/*点击忘记密码跳转至忘记密码页面*/
+//function toForgetPwd() {
+//	mui.openWindow({
+//		url : "forget_password.html"
+//	});
+//}
+
+
+
+
+//mui.plusReady(function(){
+//	mui("#reg").on("tap",function(){
+//		mui.openWindow({
+//			url : "forget_password.html"
+//		})
+//	});
+//});
+
+//mui.init({
+//pullRefresh : {
+//  container:"#refreshContainer",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+//  down : {
+//    style:'circle',//必选，下拉刷新样式，目前支持原生5+ ‘circle’ 样式
+//    color:'#2BD009', //可选，默认“#2BD009” 下拉刷新控件颜色
+//    height:'50px',//可选,默认50px.下拉刷新控件的高度,
+//    range:'100px', //可选 默认100px,控件可下拉拖拽的范围
+//    offset:'0px', //可选 默认0px,下拉刷新控件的起始位置
+//    auto: true,//可选,默认false.首次加载自动上拉刷新一次
+//    callback :pullfresh-function //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+//  }
+//}
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
